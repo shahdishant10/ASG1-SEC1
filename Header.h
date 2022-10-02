e1706cef4d2947766b26a789caade683fed90f5d
shahdishant10 <66896936+shahdishant10@users.noreply.github.com>
2022-10-02 12:44:17 -0600

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <regex>
+#include<string>
+using namespace std;
+bool isNumber(string x)
+{
+    regex e ("\\d+");
+    if (regex_match (x,e))
+        return true;
+    else
+        return false;
+}
+float Average(int arr[], int n)
+{
+    int total = 0;
+    float avgg;
+    for (int i =0; i < n; i++)
+    {
+
+        total = total + arr[i];
+        avgg = (float)total / n;                                            //for typecasting  average to float type.
+    }
+    return avgg;
+}
+
+string studentgrade(float a)          //It is use to grade the student on the basis of the average. It take Average of the student as input and return grade as output
+{
+
+    if (a >= 95 && a <= 100)
+        return "A";
+    else if (a >= 91.67 && a < 95)
+        return "A-";
+    else if (a >= 88.3 && a < 91.67)
+        return "B+";
+    else if (a >= 85 && a < 88.3)
+        return "B";
+    else if (a >= 81.67 && a < 85)
+        return "B-";
+    else if (a >= 78.33 && a < 81.67)
+        return "C+";
+    else if (a >= 75 && a < 78.33)
+        return "C";
+    else if (a >= 71.67 && a < 75)
+        return "C-";
+    else if (a >= 68.33 && a < 71.67)
+        return "D+";
+    else if (a >= 65 && a < 68.33)
+        return "D";
+    else if (a >= 61.67 && a < 65)
+        return "D-";
+    else if (a >= 0 && a < 61.67)
+        return "F";
+    else
+        return "Invalid Grade";
+}
---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include"Header.h"
+
+int main()
+{
+    ifstream filein;                                                           //To read the input file
+    ofstream fileout;                                                          //To write the file
+    string line,word,fname,lname,mname;                                        //Variable declaration of string datatype
+    float Avg;                                                                 //Variable declaration of float datatype
+    string G;                                                                  //Variable declaration of string datatype
+    int cnt,value;
+
+    filein.open("grades.txt");                                                 //To read the input file grades.txt
+    fileout.open("Result.txt");                                                //Output will be stored in result.txt
+    if(!filein.is_open())                                                      //Loop if file does not open
+    {
+        cout<<"File Cant be Opened \n";
+    return -1;
+    }
+
+   while(getline(filein,line))                                                 //Loop to read the file till the end of the file
+    {
+        istringstream in(line);
+        in>>fname>>lname>>mname;
+       if(isNumber(mname))
+       {
+           cnt=stoi(mname);
+           mname=" ";
+       }
+       else
+        in>>cnt;
+int marks[cnt];
+for(int i=0;i<cnt;i++)                                                         //To read the marks
+{
+    in>>value;
+    marks[i]=value;
+}
+        Avg = Average(marks,cnt);                                             //To store average
+        G = studentgrade(Avg);                                                //To store grade
+        cout<< "Student Name is " <<fname<<" "<<lname<<" "<<mname <<" and number of exams are:- " <<cnt<< endl;
+        cout<<"The marks for exams are :-"<<endl;
+        for (int i = 0; i < cnt; i++)                                        //Loop to print marks in console
+            cout << marks[i] << endl;
+
+        cout << "OVERALL GRADE:- " << G << endl;                             //Overall grade is printed
+        cout<< endl;
+        fileout<< "Student Name is " <<fname<<" "<<lname<<" "<<mname <<" and number of exams are:- " <<cnt<< endl;
+        fileout<<"The marks for exams are :-"<<endl;
+        for (int i = 0; i < cnt; i++)                                        //Loop to print marks in file
+        {
+            fileout << marks[i] << endl;
+        }
+        fileout << "OVERALL GRADE:- " << G << endl;
+        fileout<< endl;
+
+
+    }
+
+filein.close();                                                             //Close the file
+fileout.close();
+
+return 0;
+}
